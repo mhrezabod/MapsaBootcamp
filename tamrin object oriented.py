c0e7d540841536a31155e4369a753d594b257449
Mohammad reza bod <mhreza.bod@gmail.com>
2022-03-10 04:56:12 +0330

    oop

---
+++ tamrin object oriented.py
@@ -0,0 +1,14 @@
+import re
+
+class Account:
+    
+    def __init__(self,username,password,phone_number,email):
+
+        self.username = username
+        self.password = password
+        self.phone_number = phone_number
+        self.email = email
+
+
+
+
