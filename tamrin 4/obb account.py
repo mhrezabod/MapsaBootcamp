5894653a4abc62018d1556a1b731e16c68cc8b30
Mohammad reza bod <mhreza.bod@gmail.com>
2022-03-12 22:22:14 +0330

    final

--- tamrin 4/obb account.py
+++ tamrin 4/obb account.py
@@ -13,13 +13,13 @@ class Account:
     @staticmethod
     def check_username (username):
         if not re.search("^[a-zA-Z]+_[a-zA-Z]+$",username):
-            raise "invalid username"
+            raise Exception ("invalid username")
         return username
 
     @staticmethod
     def check_password (password):
         if not re.search("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$",password):
-            raise "invalid passwrod"
+            raise Exception ("invalid password")
         
         password = hashlib.sha256(password.encode('utf-8'))
 
@@ -30,7 +30,7 @@ class Account:
     @staticmethod
     def check_phone_number (phone_number):
         if not re.search("^((\+989)|(09))+[0-3,9]\d{8}$",phone_number):
-            raise "invalid Phone number" 
+            raise Exception ("invalid phone number") 
 
         if phone_number.startswith("+98"):
             phone_number = phone_number.replace('+989','09')
@@ -41,11 +41,11 @@ class Account:
     @staticmethod
     def check_email (email):
         if not re.search("[\w.-]+@[\w.-]+\.[a-zA-Z]{2,5}",email):
-            raise "invalid email"
+            raise Exception ("invalid email")
         return email
 
 if __name__== "__main__":
-    account = Account ("al_sdi",'1sd3d2dsA','+989121124066','mohammad.reza.bod@gmail.com')
+    account = Account ("al_sdi",'1sd3d2dsA','+989112124066','mohammad.reza.bod@gmail.com')
     print(account.username)
     print(account.password)
     print(account.phone_number)
