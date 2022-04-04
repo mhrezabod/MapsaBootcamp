a6026a5bf40ffeb435da1efc2a1b857892b8640b
Mohammad reza bod <mhreza.bod@gmail.com>
2022-04-04 16:58:51 +0430

    wrong

---
+++ tamrin 5 (eyd)/local maximum.py
@@ -0,0 +1,14 @@
+def remove_local_max(lenght,list_):
+
+    count=0
+    for i in range(lenght-2):
+        if list_[i+1]>list_[i] and list_[i+1]>list_[i+2]:
+            list_[i+1]=max(list_[i],list_[i+2])
+            count+=1
+
+    return count,list_
+
+print(remove_local_max(3,[2,1,2]))
+print(remove_local_max(4,[1,2,3,1]))
+print(remove_local_max(5,[1,2,1,2,1]))
+print(remove_local_max(9,[1,2,1,3,2,3,1,2,1]))
