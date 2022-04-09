226ab4e9f58e60cc70ebe6f99099e73657af4194
Mohammad reza bod <mhreza.bod@gmail.com>
2022-04-09 15:56:12 +0430

    incomplete

--- tamrin 5 (eyd)/well defined.py
+++ tamrin 5 (eyd)/well defined.py
@@ -1,3 +1,19 @@
 def well_defined(string):
     
-    
+    if "00" or "11" not in string:
+        return True
+
+    if '?' in string:
+        for i in (len(string)):
+            
+            list1= string[:i] + '1' + string[:i+1]
+            well_defined(list1)
+
+            list2= string[:i] + '0' + string[:i+1]
+            well_defined(list2)
+    else:
+        return False
+
+
+print(well_defined('10?0'))
+
